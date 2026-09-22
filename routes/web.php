8d5a265de7d0653c2372448434f48614c9838fc6
allera123123 <alleramike2@gmail.com>
2026-09-22 07:39:54 +0000

    Add basic student and subject routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students', function () {
+    return 'Hello Student';
+});
+
+Route::get('/subjects', function () {
+    return 'Hello Subject';
+});
