965c8eb53ed74f16fa55fb59665b4a7d7c3a682d
allera123123 <alleramike2@gmail.com>
2026-09-23 03:47:42 +0000

    Connect student and subject controllers

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/students', function () {
-    return 'Hello Student';
-});
+Route::get('/students', [StudentController::class, 'index']);
 
 Route::get('/subjects', function () {
     return 'Hello Subject';
